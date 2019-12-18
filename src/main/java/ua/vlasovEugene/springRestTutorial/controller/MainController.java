19b4b1c6195670f3e35e3eb753905c1a8f130846
Yevhen Vlasov <yevhen_vlasov@epam.com>
2019-12-18 16:26:21 +0200

    add Service-lay

--- src/main/java/ua/vlasovEugene/springRestTutorial/controller/MainController.java
+++ src/main/java/ua/vlasovEugene/springRestTutorial/controller/MainController.java
@@ -1,12 +1,6 @@
 package ua.vlasovEugene.springRestTutorial.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,56 +11,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.vlasovEugene.springRestTutorial.entity.User;
-import ua.vlasovEugene.springRestTutorial.exception.UserIsUnexistException;
+import ua.vlasovEugene.springRestTutorial.service.UserService;
 
 @RestController
 @RequestMapping("vlasov")
+@AllArgsConstructor
 public class MainController {
 
-  private final String EXCEPTION_MESSAGE = "User wit this id is not exist";
-  private AtomicInteger counter = new AtomicInteger(3);
-
-  private Map<Integer, User> inMemoryDb = Stream.of(new Object[][]{
-          {1, new User("John", "Smith")},
-          {2, new User("Edward", "Tich")},
-          {3, new User("Mike", "Jagger")}
-      }
-  ).collect(Collectors.toMap(data -> (Integer) data[0], data -> (User) data[1]));
+  private final UserService service;
 
   @GetMapping("/{id}")
   public User getUserById(@PathVariable Integer id) {
-    return Optional.of(inMemoryDb.get(id))
-        .orElseThrow(() -> new UserIsUnexistException(EXCEPTION_MESSAGE));
+    return service.getUserById(id);
   }
 
   @GetMapping
   public List<User> getAllUsers() {
-    return new ArrayList<>(inMemoryDb.values());
+    return service.getAllUsers();
   }
 
   @PostMapping
   public User addNewUser(@RequestBody User newUser) {
-    inMemoryDb.put(counter.incrementAndGet(), newUser);
-    return newUser;
+    return service.addNewUser(newUser);
   }
 
   @DeleteMapping("/{id}")
   public void deleteUserById(@PathVariable Integer id) {
-    if (inMemoryDb.containsKey(id)) {
-      inMemoryDb.remove(id);
-    } else {
-      throw new UserIsUnexistException(EXCEPTION_MESSAGE);
-    }
+    service.deleteUserById(id);
   }
 
   @PutMapping("/{id}")
   public User updateUserById(@PathVariable Integer id, @RequestBody User userForUpdate) {
-    if (inMemoryDb.containsKey(id)) {
-      User userFromDb = inMemoryDb.get(id);
-      inMemoryDb.replace(id, userForUpdate);
-      return userFromDb;
-    } else {
-      throw new UserIsUnexistException(EXCEPTION_MESSAGE);
-    }
+    return service.updateUserById(id, userForUpdate);
   }
 }
